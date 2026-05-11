724c4391264f9e72ab8f19ef563e39ebaafce17e
mkichenin-svg <mkichenin@mden-reunion.fr>
2026-05-11 08:55:28 +0400

    asr

--- app/app.R
+++ app/app.R
@@ -1,24 +1,106 @@
 # Charger les packages ------------------------------------------
 
 library(shiny)
+library(tidyverse)
+library(plotly)
+library(reactable)
+library(bslib)
+library(bsicons)
+library(shiny)
+library(reticulate)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(maps)
+library(mapdata)
+library(treemap)
+library(treemapify)
+library(cowplot)
+library(ggplot2)
+library(sf)
+library(viridisLite)
+library(here)
+library(jsonlite)
 
 
-# Define UI for app that draws a histogram ----
-ui <- fluidPage(
-  # App title ----
-  title = "Hello Shiny!",
-  # Sidebar panel for inputs ----
-    # Input: Slider for the number of bins ----
-    sliderInput(
-      inputId = "bins",
-      label = "Number of bins:",
-      min = 1,
-      max = 50,
-      value = 30
+ui <- page_navbar( 
+  title =  "Observatoire ASR ",
+  theme = bs_theme(bg = "white", fg = "black", primary = "blue",
+                   base_font = font_google("Space Mono"),
+                   code_font = font_google("Space Mono")),
+  
+  sidebar =  sidebarPanel(
+    
+    width = 250,
+    
+    imageOutput("image", height = 150), 
+    
+    checkboxGroupInput(
+      "checkGroup",
+      "Segment d'achat",
+      choices = list("Travaux" = 1, "Services" = 2, "Fournitures" = 3, "Prestations intellectuelles" = 4),
+      selected = 1),
+    
+    selectInput(
+      "zone",
+      "Sélectionnez la zone d'activité",
+      choices = c("Nord", "Sud", "Ouest", "Est")),
+    
+    plotOutput("map", width = 250, height = 250)
+    
+    
+  ),
+  
+  
+  
+  # Première page
+  
+  nav_panel("", p(
+    
+    layout_column_wrap(
+      value_box(title = "", value = "", height = 50),
+      value_box(title = "", value = ""),
+      value_box(title = "", value = ""),
     ),
-  # Output: Histogram ----
-  plotOutput(outputId = "distPlot")
+    
+    layout_column_wrap(
+      
+      card(
+        card_header(""),
+        
+      ),
+      
+      card(
+        card_header("")
+      ),
+      
+      card(
+        card_header("")
+      ),
+      
+      card(
+        card_header("")
+      )
+      
+      
+    ))),
+  
+  # Deuxième page
+  
+  nav_panel("", p("Content for Page ")),
+  
+  # Troisième page
+  
+  nav_panel("", p(
+    
+    
+    
+  ))
+  
+  # fin corps
+  
+  
 )
+
 
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
