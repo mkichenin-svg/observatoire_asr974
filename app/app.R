31593eb2fb7f0b484d71c90a72199a29f910ef67
mkichenin-svg <mkichenin@mden-reunion.fr>
2026-05-13 09:52:05 +0400

    asr

--- app/app.R
+++ app/app.R
@@ -56,7 +56,7 @@ ui <- page_navbar(
   nav_panel("Statistiques générales", p(
     
     layout_column_wrap(height = "1px",
-      value_box(title = "NOMBRE DE STRUCTURES INTEROGÉES ET CONCERNÉES", value = "0", height = 100, theme = "blue",showcase = bsicons::bs_icon('building-check')),
+      value_box(title = "NOMBRE DE STRUCTURES INTERROGÉES ET CONCERNÉES", value = "0", height = 100, theme = "blue",showcase = bsicons::bs_icon('building-check')),
       value_box(title = "NOMBRE DE MARCHÉS ATTRIBUÉS EN 2025", value = "0", height = 100, theme = "blue"),
      
     ),
@@ -111,7 +111,7 @@ ui <- page_navbar(
       ),
       
       card(
-        card_header("Bénéficaires provenant des QPV"),
+        card_header("Bénéficiaires provenant des QPV"),
         plotOutput("", height = 85)
       ),
       
@@ -120,7 +120,7 @@ ui <- page_navbar(
     layout_column_wrap(
       
       card(
-        card_header("Niveau de qualification des bénérficaires"),
+        card_header("Niveau de qualification des bénéficiaires"),
         plotOutput("", height = 85)
       ),
       
@@ -183,9 +183,9 @@ ui <- page_navbar(
   nav_panel("La clause de stage", p(
     
     layout_column_wrap(height = "1px",
-                       value_box(title = "NOMBRE DE MARCHÉS AYANT GÉNÉRÉS DES HEURES DE STAGE EN 2025", value = "0", height = 200, theme = "blue"),
+                       value_box(title = "NOMBRE DE MARCHÉS AYANT GÉNÉRÉ DES HEURES DE STAGE EN 2025", value = "0", height = 200, theme = "blue"),
                        value_box(title = "NOMBRE D'HEURES DE STAGE RÉALISÉES EN 2025", value = "0", height = 200, theme = "teal",showcase = bsicons::bs_icon('clock-history')),
-                       value_box(title = "NOMBRE DE STAGIAIRE EN 2025", value = "0",height = 200, theme = "teal",showcase = bsicons::bs_icon('person-check')),
+                       value_box(title = "NOMBRE DE STAGIAIRES EN 2025", value = "0",height = 200, theme = "teal",showcase = bsicons::bs_icon('person-check')),
     ),  
     
     
@@ -220,13 +220,9 @@ ui <- page_navbar(
     
     
     
-  )),
-  
-  # page 6
-  
-  nav_panel("SPASER", p(
-    
   ))
+  
+
   
   # fin corps
   
@@ -252,34 +248,33 @@ output$image <- renderImage(
 output$map <- renderPlot({ 
     
     
-    r <-  ggplot() + geom_sf(data=shapefile, color = "black") +
-      geom_sf(data= shapefile[5,], color = "black")+
-      geom_sf(data= shapefile[2,], color = "black")+
-      geom_sf(data= shapefile[1,], color = "black")+
-      geom_sf(data= shapefile[3,], color = "black")+
-      geom_sf(data= shapefile[4,],  color = "black")+
-      geom_sf(data= shapefile[6,],  color = "black")+
-      geom_sf(data= shapefile[7,],  color = "black")+
-      geom_sf(data= shapefile[8,],  color = "black")+
-      geom_sf(data= shapefile[9,],  color = "black")+
-      geom_sf(data= shapefile[19,], color = "black")+
-      geom_sf(data= shapefile[15,],  color = "black")+
-      geom_sf(data= shapefile[22,],  color = "black")+
-      geom_sf(data= shapefile[16,],  color = "black")+
-      geom_sf(data= shapefile[13,],  color = "black")+
-      geom_sf(data= shapefile[24,],  color = "black")+
-      geom_sf(data= shapefile[23,],  color = "black")+
-      geom_sf(data= shapefile[14,],  color = "black")+
-      geom_sf(data= shapefile[12,],  color = "black")+
-      geom_sf(data= shapefile[21,],  color = "black")+
-      geom_sf(data= shapefile[10,], color = "black")+
-      geom_sf(data= shapefile[11,],  color = "black")+
-      geom_sf(data= shapefile[17,],  color = "black")+
-      geom_sf(data= shapefile[18,],  color = "black")+
-      geom_sf(data= shapefile[20,],  color = "black")+
+    r <-   ggplot() + geom_sf(data=shapefile, color = "black", fill="royalblue") +
+      geom_sf(data= shapefile[5,], color = "black", fill="royalblue")+
+      geom_sf(data= shapefile[2,], color = "black", fill="royalblue")+
+      geom_sf(data= shapefile[1,], color = "black", fill="royalblue")+
+      geom_sf(data= shapefile[3,], color = "black", fill="royalblue")+
+      geom_sf(data= shapefile[4,],  color = "black", fill="royalblue")+
+      geom_sf(data= shapefile[6,],  color = "black", fill="royalblue")+
+      geom_sf(data= shapefile[7,],  color = "black", fill="royalblue")+
+      geom_sf(data= shapefile[8,],  color = "black", fill="royalblue")+
+      geom_sf(data= shapefile[9,],  color = "black", fill="royalblue")+
+      geom_sf(data= shapefile[19,],color = "black",fill="royalblue")+
+      geom_sf(data= shapefile[15,],  color = "black", fill="royalblue")+
+      geom_sf(data= shapefile[22,],  color = "black", fill="royalblue")+
+      geom_sf(data= shapefile[16,],  color = "black",fill="royalblue")+
+      geom_sf(data= shapefile[13,],  color = "black",fill="royalblue")+
+      geom_sf(data= shapefile[24,],  color = "black",fill="royalblue")+
+      geom_sf(data= shapefile[23,],  color = "black",fill="royalblue")+
+      geom_sf(data= shapefile[14,],  color = "black",fill="royalblue")+
+      geom_sf(data= shapefile[12,],  color = "black",fill="royalblue")+
+      geom_sf(data= shapefile[21,],  color = "black",fill="royalblue")+
+      geom_sf(data= shapefile[10,], color = "black",fill="royalblue")+
+      geom_sf(data= shapefile[11,],  color = "black",fill="royalblue")+
+      geom_sf(data= shapefile[17,],  color = "black",fill="royalblue")+
+      geom_sf(data= shapefile[18,],  color = "black",fill="royalblue")+
+      geom_sf(data= shapefile[20,],  color = "black",fill="royalblue")+
       theme_minimal()+
-      theme(axis.text.x = element_blank(), 
-            axis.text.y = element_blank())
+      theme(axis.text.x = element_blank(), axis.text.y = element_blank())
     
     Nord <- ggplot() + geom_sf(data=shapefile, color = "black") +
       geom_sf(data= shapefile[5,], color = "black", fill="royalblue")+
@@ -325,31 +320,31 @@ output$map <- renderPlot({
       theme(axis.text.x = element_blank(), axis.text.y = element_blank())
     
     
-multi <-  ggplot() + geom_sf(data=shapefile, color = "black", fill="royalblue") +
-      geom_sf(data= shapefile[5,], color = "black", fill="royalblue")+
-      geom_sf(data= shapefile[2,], color = "black", fill="royalblue")+
-      geom_sf(data= shapefile[1,], color = "black", fill="royalblue")+
-      geom_sf(data= shapefile[3,], color = "black", fill="royalblue")+
-      geom_sf(data= shapefile[4,],  color = "black", fill="royalblue")+
-      geom_sf(data= shapefile[6,],  color = "black", fill="royalblue")+
-      geom_sf(data= shapefile[7,],  color = "black", fill="royalblue")+
-      geom_sf(data= shapefile[8,],  color = "black", fill="royalblue")+
-      geom_sf(data= shapefile[9,],  color = "black", fill="royalblue")+
-      geom_sf(data= shapefile[19,],color = "black",fill="royalblue")+
-      geom_sf(data= shapefile[15,],  color = "black", fill="royalblue")+
-      geom_sf(data= shapefile[22,],  color = "black", fill="royalblue")+
-      geom_sf(data= shapefile[16,],  color = "black",fill="royalblue")+
-      geom_sf(data= shapefile[13,],  color = "black",fill="royalblue")+
-      geom_sf(data= shapefile[24,],  color = "black",fill="royalblue")+
-      geom_sf(data= shapefile[23,],  color = "black",fill="royalblue")+
-      geom_sf(data= shapefile[14,],  color = "black",fill="royalblue")+
-      geom_sf(data= shapefile[12,],  color = "black",fill="royalblue")+
-      geom_sf(data= shapefile[21,],  color = "black",fill="royalblue")+
-      geom_sf(data= shapefile[10,], color = "black",fill="royalblue")+
-      geom_sf(data= shapefile[11,],  color = "black",fill="royalblue")+
-      geom_sf(data= shapefile[17,],  color = "black",fill="royalblue")+
-      geom_sf(data= shapefile[18,],  color = "black",fill="royalblue")+
-      geom_sf(data= shapefile[20,],  color = "black",fill="royalblue")+
+multi <-  ggplot() + geom_sf(data=shapefile, color = "black", fill="orange") +
+      geom_sf(data= shapefile[5,], color = "black", fill="orange")+
+      geom_sf(data= shapefile[2,], color = "black", fill="orange")+
+      geom_sf(data= shapefile[1,], color = "black", fill="orange")+
+      geom_sf(data= shapefile[3,], color = "black", fill="orange")+
+      geom_sf(data= shapefile[4,],  color = "black", fill="orange")+
+      geom_sf(data= shapefile[6,],  color = "black", fill="orange")+
+      geom_sf(data= shapefile[7,],  color = "black", fill="orange")+
+      geom_sf(data= shapefile[8,],  color = "black", fill="orange")+
+      geom_sf(data= shapefile[9,],  color = "black", fill="orange")+
+      geom_sf(data= shapefile[19,],color = "black",fill="orange")+
+      geom_sf(data= shapefile[15,],  color = "black", fill="orange")+
+      geom_sf(data= shapefile[22,],  color = "black", fill="orange")+
+      geom_sf(data= shapefile[16,],  color = "black",fill="orange")+
+      geom_sf(data= shapefile[13,],  color = "black",fill="orange")+
+      geom_sf(data= shapefile[24,],  color = "black",fill="orange")+
+      geom_sf(data= shapefile[23,],  color = "black",fill="orange")+
+      geom_sf(data= shapefile[14,],  color = "black",fill="orange")+
+      geom_sf(data= shapefile[12,],  color = "black",fill="orange")+
+      geom_sf(data= shapefile[21,],  color = "black",fill="orange")+
+      geom_sf(data= shapefile[10,], color = "black",fill="orange")+
+      geom_sf(data= shapefile[11,],  color = "black",fill="orange")+
+      geom_sf(data= shapefile[17,],  color = "black",fill="orange")+
+      geom_sf(data= shapefile[18,],  color = "black",fill="orange")+
+      geom_sf(data= shapefile[20,],  color = "black",fill="orange")+
       theme_minimal()+
       theme(axis.text.x = element_blank(), axis.text.y = element_blank())
     
